952a79e58208a48d97afed6ae6fed2e12876ece3
barmstrong <barmstrong@cfday.net>
2022-07-20 22:20:06 -0400

    Created a "runner" that is used by the Cloud Function when triggered.

---
+++ python/flightStreamingRunner.py
@@ -0,0 +1,15 @@
+import functions_framework
+from flight.stream.openSkyParser import parse
+
+@functions_framework.http
+def runner(request):
+  """HTTP Cloud Function.
+  Args:
+      request (flask.Request): The request object.
+      <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+  Returns:
+      The response text, or any set of values that can be turned into a
+      Response object using `make_response`
+      <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+  """
+  return parse(request)
