cd9240fe5e8ef88aa8d4bc92de6f3c1c8e4cdbbe
barmstrong <barmstrong@cfday.net>
2022-08-08 19:01:57 -0400

    Had to make use of the getMessageJSON function to pull the message out of the request, otherwise would not recognize the contents that we pass into the cloud function. Also added a addTimestamp parameter so that the function will store the files in a subfolder with a timestamp each time it is run. (Otherwise will overwrite the data.) Put in some error checks for when the bucket or file of allStocks is not present.

--- python/api/stocks/yahooFinance.py
+++ python/api/stocks/yahooFinance.py
@@ -1,8 +1,39 @@
+# The following code retrieves stock open/close data using a Yahoo Finance library and produces rows as JSON objects that can be
+# loaded directly into a Big Query table in Google Cloud.
+# This file is set up to work with Cloud Function. NOTE: The cloud function needs enough memory to download the
+# stock data. I needed at least an instance with 256MB of memory to process a period of 7d.
+#
+# You can configure how the code is run using the message that is passed in when the cloud function is triggered:
+#   debug: will spit out occassional debug statements. You can turn off debugging by setting it to 0.
+#   projectId: the ID of your project.
+#   bucket: Google Cloud Storage bucket for storing the data; defaults to projectId + "_data".
+#   path: path within the bucket to process the data in (defaults to "stocks".)
+#   period: defaults to 10y.
+#   interval: defaults to 1 day ("1d").
+#   addTimestamp: if "true" then place all the files within a folder named by a timestamp, otherwise will overwrite any file with the same name in the path you give.
+#
+# You can test out this code from the command-line:
+#   Make sure to set your PYTHONPATH to include the code, such as the following for a LINUX system, such as from Cloud Shell:
+#      PYTHONPATH=~/classResources/python python ~/classResources/python/api/stocks/yahooFinance.py -projectId prof-big-data -bucket prof-big-data_data -path week-of-stocks -interval 7d -period 1d
+# Use the following tests from the Cloud Function UI:
+#   Write to Google Storage and publish to a Pub/Sub queue simultaneously:
+#   {
+#        "debug":10,
+#        "bucket":"prof-big-data_data",
+#        "path":"stocks",
+#        "projectId":"prof-big-data",
+#        "interval":"1d",
+#        "period":"7d",
+#        "addTimestamp":"true"
+#   }
+
 import yfinance as yf
 from argparse import ArgumentParser
+import functions_framework
 import os
 import json
 import logging
+from datetime import datetime
 from google.cloud import storage
 from google.cloud.pubsub_v1 import PublisherClient
 
@@ -10,7 +41,7 @@ logging.basicConfig(format='%(asctime)s.%(msecs)03dZ,%(pathname)s:%(lineno)d,%(l
                     datefmt="%Y-%m-%d %H:%M:%S")
 _logger = logging.getLogger(__name__)
 
-_allStocksFile='jp_files/Programs/stocks.csv'
+_allStocksFile='allStocks.csv'
 _storageClient=None
 
 def _getStorageClient(bucket):
@@ -23,6 +54,7 @@ def _getStorageClient(bucket):
   if _storageClient is None:
     # This is the first time we are hitting storage, so open a new connection.
     _storageClient=storage.Client().bucket(bucket)
+  if not _storageClient.exists(): raise Exception('Cannot access bucket '+bucket)
   return _storageClient
 
 def _store(bucket,path,data):
@@ -67,11 +99,23 @@ def _parse(stock,period,interval,action):
   return action(yf.download(tickers=stock, period=period, interval=interval))
 
 def parseAll(allStocksFile,bucket,path,period,interval):
-  for symbol in _getStorageClient(bucket).blob(_allStocksFile).download_as_bytes().decode('utf-8').split('\n'):
-    cleanedSymbol=symbol.strip()
-    _logger.debug('Parsing '+cleanedSymbol)
-    action=lambda data: _store(bucket,path+'/'+cleanedSymbol+'.csv',data)
-    _parse(cleanedSymbol,period,interval,action)
+  numStocks=0
+  stocksFileContents=_getStorageClient(bucket).blob(_allStocksFile)
+  if stocksFileContents.exists():
+    symbols=stocksFileContents.download_as_bytes().decode('utf-8').split('\n')
+  else:
+    _logger.error('Cannot read stocks from '+allStocksFile+' in bucket '+bucket)
+    symbols=['GOOGL','GLD','NFLX']
+  for symbol in symbols:
+    try:
+      cleanedSymbol=symbol.strip()
+      _logger.debug('Parsing '+cleanedSymbol)
+      action=lambda data: _store(bucket,path+'/'+cleanedSymbol+'.csv',data)
+      _parse(cleanedSymbol,period,interval,action)
+      numStocks+=1
+    except:
+      _logger.error('Cannot parse stocks for symbol '+symbol)
+  return numStocks
 
 def _getMessageJSON(request):
   '''
@@ -117,17 +161,34 @@ def _getMessageJSON(request):
     messageJSON=message
   return messageJSON
 
+@functions_framework.http
 def entry(request):
-  defaultProjectId=os.environ.get('GOOGLE_CLOUD_PROJECT','no_project')
-  _logger.debug('request is '+str(request)+' with args '+str(request.args))
-  request_json=request.get_json()
-  message={}
-  if request_json is not None and 'message' in request_json: message=request_json['message']
-  bucket=message.get('bucket',defaultProjectId+'_data')
+  '''
+  Args:
+    request: the request is passed into the cloud function and message will have the JSON that the funciton is triggered with.
+  '''
+  _logger.setLevel(10)
+  message=_getMessageJSON(request)
+
+  debug=message.get('debug', 10)
+  if debug==0:
+    debug=None
+  else:
+    _logger.setLevel(debug)
+
+  projectId=message.get('projectId',os.environ.get('GOOGLE_CLOUD_PROJECT','no_project'))
+  bucket=message.get('bucket',projectId+'_data')
   path=message.get('path','stocks')
   period=message.get('period','10y')
   interval=message.get('interval','1d')
-  parseAll(_allStocksFile,bucket,path,period,interval)
+  addTimestamp=message.get('addTimestamp',None)
+  if addTimestamp=='true':
+    # Append a timestamp to the path so that we don't overwrite an existing set of files.
+    path+='/timestamp='+datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+  _logger.info('Will parse all stocks in '+_allStocksFile+' for the period of '+period+' at the interval of '+interval+
+               ', storing in path '+path+' of bucket '+bucket+', projectId='+str(projectId))
+  numParsed=parseAll(_allStocksFile,bucket,path,period,interval)
+  return 'Completed parsing '+str(numParsed)+' stocks.'
 
 if __name__ == '__main__':
   '''
@@ -138,11 +199,13 @@ if __name__ == '__main__':
   PYTHONPATH at the beginning of the statement to execute, like so:
       PYTHONPATH=~/classResources/python python ~/classResources/python/api/stocks/yahooFinance.py -h
   '''
-  defaultProjectId=os.environ.get('GOOGLE_CLOUD_PROJECT','no_project')
   parser=ArgumentParser()
-  parser.add_argument('-bucket',default=defaultProjectId+'_data')
+  parser.add_argument('-bucket',default=None)
   parser.add_argument('-path',default='stocks')
   parser.add_argument('-period',default='10y')
   parser.add_argument('-interval',default='1d')
+  parser.add_argument('-projectId',default=None)
   args = parser.parse_args()
+  projectId=os.environ.get('GOOGLE_CLOUD_PROJECT','no_project') if args.projectId is None else args.projectId
+  bucket=projectId+'_data' if args.bucket is None else args.bucket
   parseAll(_allStocksFile,args.bucket,args.path,args.period,args.interval)
